eede5f127af8371547fcf9f700e8beca4d9ba24f
Olivia-Chen-Xu <oliviachenxu@gmail.com>
2021-10-17 10:48:29 -0400

    all uptodate

--- app/lib/MainPages/home.dart
+++ app/lib/MainPages/home.dart
@@ -3,14 +3,11 @@ import 'package:healthreminders/MainPages/Medicine.dart';
 import 'package:healthreminders/MainPages/MoreOptions.dart';
 import 'package:healthreminders/MainPages/HomePage.dart';
 import 'package:healthreminders/MainPages/MoreReminders.dart';
-
+import 'package:healthreminders/MainPages/ShoppingCart.dart';
 
 //final FirebaseAuth _auth = FirebaseAuth.instance;
 
 class Home extends StatefulWidget {
-
-
-
   @override
   HomeState createState() => HomeState();
 }
@@ -22,50 +19,46 @@ class HomeState extends State<Home> {
     HomePage(),
     Medicine(),
     MoreReminders(),
+    ShoppingCart(),
     MoreOptions(),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-      body: Center(
-        child: _pageOptions[_selectedTab],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-          currentIndex: _selectedTab,
-      // this will be set when a new tab is tapped
-      items: [
-        BottomNavigationBarItem(
-          icon:  Icon(Icons.home),
-          title: Text('Home'),
+        body: Center(
+          child: _pageOptions[_selectedTab],
         ),
-        BottomNavigationBarItem(
-          icon:  ImageIcon(AssetImage('assets/pill.png')),
-          title: Text('Medicines'),
-        ),
-        BottomNavigationBarItem(
-            icon: Icon(Icons.alarm),
-            title: Text('More Reminders')
-        ),
-        BottomNavigationBarItem(
-        icon:  ImageIcon(AssetImage('assets/Images/dots.png')),
-        title: Text('More Options')
-        )
-  ],
-        selectedItemColor: Colors.teal,
-        unselectedItemColor: Colors.grey[800],
-        unselectedLabelStyle: TextStyle(color:Colors.black),
-
-    onTap: (int index) {
-      setState(() {
-        _selectedTab = index;
-      },
-
-
-      );
-    })
-            );
-
+        bottomNavigationBar: BottomNavigationBar(
+            currentIndex: _selectedTab,
+            // this will be set when a new tab is tapped
+            items: [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                title: Text('Home'),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.search),
+                title: Text('Search'),
+              ),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.camera), title: Text('Camera')),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.shopping_cart),
+                  title: Text('Shopping Cart')),
+              BottomNavigationBarItem(
+                  icon: ImageIcon(AssetImage('assets/Images/dots.png')),
+                  title: Text('More Options'))
+            ],
+            selectedItemColor: Colors.teal,
+            unselectedItemColor: Colors.grey[800],
+            unselectedLabelStyle: TextStyle(color: Colors.black),
+            onTap: (int index) {
+              setState(
+                () {
+                  _selectedTab = index;
+                },
+              );
+            }));
   }
 }
