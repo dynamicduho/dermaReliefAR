687cf6ce68fd7ca20302561833afe42e327c6958
CodeSarthak <getsarthakaggarwal@gmail.com>
2021-10-17 13:42:18 +0530

    deleted testfile and copied Olivia's code

---
+++ derma-relief-main/chatsnap/lib/ui/cart_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class cartPage extends StatelessWidget {
+  const cartPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
